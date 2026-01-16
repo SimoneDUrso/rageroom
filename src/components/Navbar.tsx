b87b39a473c90beeea2f5609e2a98db83da81d74
Simone D'Urso <s.durso@tri-tech.it>
2026-01-16 11:02:37 +0100

    [FIX] Fixed import path in Navbar file

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Booking from './pages/Booking';
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Booking from '../pages/Booking';
 import { ShoppingBag, Menu } from 'lucide-react';
 
 function App() {
